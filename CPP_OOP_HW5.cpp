8b0641ba23b59090e0ec528ab2de9b6d672f336c
VLADIMIR <veselov2100@mail.ru>
2021-09-13 10:54:23 +0300

    Complete

--- CPP_OOP_HW5.cpp
+++ CPP_OOP_HW5.cpp
@@ -1,20 +1,85 @@
-// CPP_OOP_HW5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "BlackJack.h"
+
+//Task 1 ===================================================
+template <class T>
+class Pair1
+{
+private:
+    T a, b;
+public:
+    Pair1(T input1,T input2)
+        : a(input1), b(input2){};
+
+    T first() const
+    {
+        return a;
+    }
+
+    T second() const
+    {
+        return b;
+    }
+};
+
+//Task 2 ==================================================
+template <typename T1, typename T2>
+class Pair
+{
+private:
+    T1 a;
+    T2 b;
+public:
+    Pair(){}
+    Pair(T1 inputA, T2 inputB)
+        : a(inputA), b(inputB) {}
+
+    T1 first() const
+    {
+        return a;
+    }
+
+    T2 second() const
+    {
+        return b;
+    }
+};
+
+//Task 3 ==========================================================
+
+template <typename T>
+class StringValuePair : public Pair<std::string, T>
+{
+public:
+    StringValuePair(std::string inputString, T inputValue)
+        : Pair<std::string, T>(inputString, inputValue){}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Task 1 ==========================================================
+
+    Pair1<int> p1(6, 9);
+    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair1<double> p2(3.4, 7.8);
+    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+    //Task 2 ==========================================================
+
+    Pair<int, double> p3(6, 7.8);
+    std::cout << "Pair: " << p3.first() << ' ' << p3.second() << '\n';
+
+    const Pair<double, int> p4(3.4, 5);
+    std::cout << "Pair: " << p4.first() << ' ' << p4.second() << '\n';
+
+    //Task 3 ==========================================================
+
+    StringValuePair<int> svp("Amazing", 7);
+    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
